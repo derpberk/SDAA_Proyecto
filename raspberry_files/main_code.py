a2264fe4a5d0f11c168114e9696ba5921313bb87
Samuel <derpberk@gmail.com>
2019-12-19 16:20:40 +0100

    Comunicacion Raspberry mediante MQTT
    Se ha cambiado la comunicacion mediante HTML msgs a una lectura de la red MQTT

--- raspberry_files/main_code.py
+++ raspberry_files/main_code.py
@@ -1,37 +1,64 @@
 #!/usr/bin/env python
 
+# Programa principal de comunicacion con el uCARE Glove #
 import requests
 import time
+import paho.mqtt.client as mqtt
+import printlog.printlog as printlog # Funcion especial para imprimir logs creada para la ocasion #
+# Parametros de conexion #
 
+CLIENT_ID = "Raspbi_ID"
+BROKER_LOCALHOST_IP = "192.168.1.137"
+DEVICE_IP = "192.168.1.255" 
+ACC_SENSOR_TOPIC = "sensors/acc"
+PULSE_SENSOR_TOPIC = "sensors/pulse"
+OXI_SENSOR_TOPIC = "sensors/oxigen"
+TM = 2
 
-ip_dispositivo = "192.168.1.200"
+# Ip del dispositivo del Glove #
+
+ip_mqtt_broker = LOCALHOST_IP
+ip_dispositivo = DEVICE_IP
 conf_route = "/r_u_here"
-
+FLAG_new_data = False
 
 # Funcion principal #
 if __name__ == "__main__":
     
-    """ Así es cómo se captura un html 
-        url = 'https://www.google.com/'
-        r = requests.get(url)
-        r.text
-    """
-    num_failed_conection = 0
-    while(num_failed_conection < 10):
-        time.sleep(5)
-        try:
-            resp = requests.get("http://" + ip_dispositivo + conf_route)
-        except:
-            num_failed_conection += 1
-            print("Intento " + str(num_failed_conection) + "...")
+    try:
+        printlog("Creando cliente MQTT ...","INFO")
+        client = mqtt.Client(CLIENT_ID)
+        printlog("Cliente MQTT creado!","OK")
+
+        printlog("Conectando a Broker MQTT","INFO")
+        client.connect(BROKER_LOCALHOST_IP)
+        # Comenzamos el loop de escucha #
+        client.loop_start() 
+        printlog("Conectado a la red MQTT!","OK")
+
+        printlog("Escuchando los topics de sensores...","INFO")
+        client.subscribe(ACC_SENSOR_TOPIC)
+        client.subscribe(PULSE_SENSOR_TOPIC)
+        client.subscribe(OXI_SENSOR_TOPIC)
+        printlog("Conectado a los sensores","OK")
     
-    # Punto de salida si no se conecta a tiempo #
-    if(num_failed_conection == 10):
-        print("Dispositivo no encontrado. Asegurese de que tiene su dispositivo conectado.")
-        exit()
+    except:
+        # Fallo de red. Troubles handling ... #
+        printlog("ERROR DE CONEXION. VERIFIQUE LA RED MQTT.","ERROR")
+
+    
+    FLAG_new_data = False
+    while(1){
+
+        # Esperamos un tiempo de recepcion TM #
+        time.sleep(TM)
+        if(FLAG_new_data): # Si hay un nuevo dato #
+            printlog("DATOS NUEVOS RECIBIDOS!!!","INFO")
+
+    }
     
     # Emitimos mensaje de conexion # 
-    print("Dispositivo uCARE conectado!")
+    printlog("Dispositivo uCARE conectado!","INFO")
 
     
 
---
+++ raspberry_files/printlog.py
@@ -0,0 +1,22 @@
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def printlog(str,type = ""):
+
+    if(type == "WARNING"):
+        print(bcolors.BOLD + bcolors.WARNING + "LOG: " + str + bcolors.ENDC)
+    elif(type == "INFO"):
+        print(bcolors.BOLD + bcolors.OKBLUE + "LOG: " + str + bcolors.ENDC)
+    elif(type == "OK"):
+        print(bcolors.BOLD + bcolors.OKGREEN + "LOG: " + str + bcolors.ENDC)
+    elif(type == "ERROR"):
+        print(bcolors.BOLD + bcolors.FAIL + "LOG: " + str + bcolors.ENDC)
+    else:
+        print( "LOG: " + str)
